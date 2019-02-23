3a84c9bb9d37746c14252e12e9e6dd3ee1090e26
Anna Penaloza <annapenaloza@Annas-MacBook-Pro.local>
2019-02-23 15:26:46 -0500

    Started the lab day 2 with conditioned questions

---
+++ main.js
@@ -0,0 +1,14 @@
+`use strict`
+// Design a guessing game about you that incolces FIVE yes/no questions. Be creative and fun and whimsical. It's your project, so it's up to you
+
+let question1 = prompt!('Is Anna my name?')
+let question2 = prompt!(`Is Stranger Things my favorite show?`)
+let question3 = prompt!(`Am I a designer?`) 
+
+if (question2 === 'Stranger Things')(
+    console.log('YES')
+)
+else(
+    console.log('NO')
+)
+
